f7407b99025a6bdfefcf23d761914a02909e0dc3
Benoit Bryon <benoit@marmelune.net>
2011-07-07 15:27:31 +0200

    Added author and author_email in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "drupal.drush.generator",
-    version = '0.1dev',
-    entry_points = {
+    name='drupal.drush.generator',
+    version='0.1dev',
+    author='Benoit Bryon',
+    author_email='benoit@marmelune.net',
+    entry_points={
         'zc.buildout': [
             'drush_generator = drupal.drush.generator.buildout:DrushGeneratorRecipe',
         ],
     },
-    scripts = [
+    scripts=[
         'drupal/drush/generator/bin/drush_generator.py',
     ],
     packages=find_packages(),
